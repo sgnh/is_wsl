5c6e750eac4cf4410001b7055d6439b5f23651b2
Steffen Hansen <sgnh@users.noreply.github.com>
2020-05-26 17:39:05 +0200

    Add tests

--- test/is_wsl_test.rb
+++ test/is_wsl_test.rb
@@ -1,11 +1,45 @@
-require "test_helper"
+# frozen_string_literal: true
+
+require 'test_helper'
 
 class IsWslTest < Minitest::Test
   def test_that_it_has_a_version_number
     refute_nil ::IsWsl::VERSION
   end
 
-  def test_it_does_something_useful
-    assert false
+  def test_wsl_is_false_on_docker
+    IsDocker.stub(:docker?, true) do
+      assert IsWsl.wsl? == false
+    end
+  end
+
+  def test_wsl_is_true_on_WSL1
+    stub = proc { |args| args == '/proc/version' ? ['Microsoft'] : (raise Errno::ENOENT) }
+
+    File.stub(:readlines, stub) do
+      assert IsWsl.wsl? == true
+    end
+  end
+
+  def test_wsl_is_true_on_WSL2
+    stub = proc { |args| args == '/proc/version' ? ['microsoft'] : (raise Errno::ENOENT) }
+
+    File.stub(:readlines, stub) do
+      assert IsWsl.wsl? == true
+    end
+  end
+
+  def test_wsl_is_false_on_linux
+    stub = proc { |args| args == '/proc/version' ? ['linux'] : (raise Errno::ENOENT) }
+
+    File.stub(:readlines, stub) do
+      assert IsWsl.wsl? == false
+    end
+  end
+
+  def test_wsl_is_false_without_version
+    File.stub(:readlines, proc { raise Errno::ENOENT }) do
+      assert IsWsl.wsl? == false
+    end
   end
 end
