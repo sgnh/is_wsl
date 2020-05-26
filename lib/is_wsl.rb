6332c597c97d5f3d9aa99154e969a31d83923084
Steffen Hansen <sgnh@users.noreply.github.com>
2020-05-26 17:39:09 +0200

    Add wsl?

--- lib/is_wsl.rb
+++ lib/is_wsl.rb
@@ -1,6 +1,13 @@
-require "is_wsl/version"
+# frozen_string_literal: true
+
+require 'is_wsl/version'
+require 'is_docker'
 
 module IsWsl
-  class Error < StandardError; end
-  # Your code goes here...
+  def self.wsl?
+    !IsDocker.docker? &&
+      !File.readlines('/proc/version').grep(/microsoft/i).empty?
+  rescue Errno::ENOENT
+    false
+  end
 end
